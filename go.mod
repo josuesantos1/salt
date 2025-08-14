a08470e75a38f51e781c7f0054cafa18555a1f65
josue santos <josue.santos.dev@gmail.com>
2025-08-14 00:28:38 -0300

    added module to brotli compression

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/josuesantos1/salt
 
 go 1.24.5
+
+require github.com/andybalholm/brotli v1.2.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/andybalholm/brotli"
 )
 
 func fileServer() http.Handler {
@@ -25,6 +28,29 @@ func LogMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(recorder, r)
 		log.Printf("%s %s %s %d", r.RemoteAddr, r.Method, r.URL.Path, recorder.status)
 	})
+}
+
+func BrotliMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "br") {
+			w.Header().Set("Content-Encoding", "br")
+			brWriter := brotli.NewWriter(w)
+			defer brWriter.Close()
+			bw := &brotliResponseWriter{ResponseWriter: w, Writer: brWriter}
+			next.ServeHTTP(bw, r)
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+type brotliResponseWriter struct {
+	http.ResponseWriter
+	Writer *brotli.Writer
+}
+
+func (w *brotliResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
 }
 
 func main() {
