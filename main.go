62a4c54557062cdcb6d09efdd71e82664b500feb
josue santos <josue.santos.dev@gmail.com>
2025-08-13 23:49:01 -0300

    added logger in project

--- main.go
+++ main.go
@@ -9,12 +9,32 @@ func fileServer() http.Handler {
 	return http.FileServer(http.Dir("./public"))
 }
 
+type statusRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+func (r *statusRecorder) WriteHeader(code int) {
+	r.status = code
+	r.ResponseWriter.WriteHeader(code)
+}
+
+func LogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		recorder := &statusRecorder{ResponseWriter: w, status: 200}
+		next.ServeHTTP(recorder, r)
+		log.Printf("%s %s %s %d", r.RemoteAddr, r.Method, r.URL.Path, recorder.status)
+	})
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("GET /", fileServer())
 
-	err := http.ListenAndServe(":1112", mux)
+	loggedMux := LogMiddleware(mux)
+
+	err := http.ListenAndServe(":1112", loggedMux)
 	if err != nil {
 		log.Fatal(err)
 	}
